89a3b5ad598fef9bdf14725f846c5f76b9979a14
sally <119037864+ht18boynd@users.noreply.github.com>
2023-10-19 12:52:28 +0700

    v
    2

--- ezd-admin/src/components/Games/Index.vue
+++ ezd-admin/src/components/Games/Index.vue
@@ -36,9 +36,16 @@
                   <h5>Create</h5>
                 </div>
                 <div class="card-body">
-                  <form class="was-validated" novalidate  @submit.prevent="createNewGame" enctype="multipart/form-data">
+                  <form
+                    class="was-validated"
+                    novalidate
+                    @submit.prevent="createNewGame"
+                    enctype="multipart/form-data"
+                  >
                     <div class="mb-3">
-                      <label class="form-label" for="nameOfGame">Name Of Game:</label>
+                      <label class="form-label" for="nameOfGame"
+                        >Name Of Game:</label
+                      >
                       <input
                         class="form-control"
                         id="nameOfGame"
@@ -46,7 +53,9 @@
                         v-model="nameGame"
                         required
                       />
-                      <div class="invalid-feedback">Please provide a Name of Game.</div>
+                      <div class="invalid-feedback">
+                        Please provide a Name of Game.
+                      </div>
                       <div class="valid-feedback">Looks good!</div>
                     </div>
                     <div class="mb-3">
@@ -55,20 +64,22 @@
                         class="form-control"
                         id="gameImage"
                         type="file"
-                        @change="onFileChange"
+                        @change="onFileChangeForCreate"
                         required
                       />
-                      <div class="invalid-feedback">Please provide an Image.</div>
+                      <div class="invalid-feedback">
+                        Please provide an Image.
+                      </div>
                       <div class="valid-feedback">Looks good!</div>
                     </div>
                     <div class="col-md-2">
                       <button
-                      class="btn btn-primary"
-                      type="submit"
-                      :disabled="!isFormValid"
-                    >
-                      Create
-                    </button>
+                        class="btn btn-primary"
+                        type="submit"
+                        :disabled="!isFormValid"
+                      >
+                        Create
+                      </button>
                     </div>
                   </form>
                 </div>
@@ -80,18 +91,19 @@
                   <h5>Edit Game</h5>
                 </div>
                 <div class="card-body">
-                  <form  class="was-validated" novalidate>
+                  <form class="was-validated" novalidate  @submit.prevent="editGame">
                     <div class="mb-3">
-                      <label class="form-label" for="validationCustom01">
-                        Name Of Game :</label
+                      <label class="form-label" for="validationCustom01"
+                        >Name Of Game:</label
                       >
                       <input
-                       
                         class="form-control"
                         id="validationCustom01"
                         type="text"
+                        v-model="nameGameForEdit"
                         required
                       />
+                      <!-- Sử dụng v-model để kết nối với nameGame -->
                       <div class="invalid-feedback">
                         Please provide a Name of Game
                       </div>
@@ -99,22 +111,30 @@
                     </div>
                     <div class="mb-3">
                       <label class="form-label" for="validationCustom05"
-                        >Image :
-                      </label>
+                        >Image:</label
+                      >
                       <input
-                      
                         class="form-control"
                         id="validationCustom05"
                         type="file"
                         required
+                        @change="onFileChangeForEdit"
                       />
                       <div class="invalid-feedback">
                         Please provide an Image.
                       </div>
                       <div class="valid-feedback">Looks good!</div>
                     </div>
+                    <div class="mb-3">
+                      <img
+                        v-if="imageGameForEdit"
+                        :src="imageGameForEdit"
+                        alt="Game Image"
+                        class="img-100 rounded-circle"
+                      />
+                    </div>
 
-                    <button  class="btn btn-primary">
+                    <button class="btn btn-primary">
                       Save Changes
                     </button>
                   </form>
@@ -140,7 +160,6 @@
                       <th>
                         <div>
                           <button
-                           
                             type="button"
                             class="btn btn-pill btn-primary btn-air-primary active"
                           >
@@ -166,20 +185,20 @@
                           @click="showImage(game.nameGame, game.imageName)"
                         />
                       </td>
-                      
+
                       <td>
-                        <a href="#">
+                        <a @click="startEditingGame(game.id)">
+                          <!-- Sử dụng sự kiện @click -->
                           <i class="icon-pencil-alt"></i>Edit
                         </a>
                       </td>
                       <td>
-                        <a  @click="deleteGame(game.id)">
+                        <a @click="deleteGame(game.id)">
                           <i class="icon-trash"></i>Delete
                         </a>
                       </td>
                     </tr>
                   </tbody>
-                  
                 </table>
               </div>
             </div>
@@ -198,22 +217,78 @@ export default {
   name: "gameCollector",
   data() {
     return {
-     gamelist:[],  
-     nameGame:'',
-     selectedFile:null,
-     editingGame: "",
-    
+      gamelist: [],
+      nameGame: "",
+      selectedFileForCreate: null,
 
+      nameGameForEdit: "", // Thêm biến nameGameForEdit
+      selectedFileForEdit: null,
+      editGameId: null,
+      imageGameForEdit: "", // Thêm biến imageGameForEdit
     };
   },
   methods: {
-
-  
-    onFileChange(event) {
-      this.selectedFile = event.target.files[0];
+    onFileChangeForEdit(event) {
+      this.selectedFileForEdit = event.target.files[0];
     },
-    async createNewGame() { try {
-        const response = await GameService.addNewGame(this.selectedFile, this.nameGame);
+    startEditingGame(id) {
+
+      this.editGameId = id;
+      // Đọc thông tin game từ danh sách và gán cho các field của form chỉnh sửa (nameGame và imageGame)
+      const selectedGame = this.gamelist.find((game) => game.id === id);
+      if (selectedGame) {
+        this.nameGameForEdit = selectedGame.nameGame;
+        this.imageGameForEdit = selectedGame.imageName; // Gán giá trị imageGameForEdit
+      }
+    },
+
+    async editGame() {
+      try {
+        // Đảm bảo rằng đã chọn game cần chỉnh sửa
+        if (this.editGameId) {
+          const response = await GameService.editGame(
+            this.editGameId,
+            this.selectedFileForEdit,
+            this.nameGameForEdit
+          );
+
+          if (response.status === 200) {
+            await this.getAllGames();
+            this.nameGameForEdit = "";
+            this.selectedFileForEdit = null;
+            this.editGameId = null; // Đặt lại giá trị ID game cần chỉnh sửa
+            Swal.fire({
+              position: "top-end",
+              icon: "success",
+              title: "Edit Success!",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+          } else {
+            Swal.fire({
+              position: "top-end",
+              icon: "error",
+              title: "Edit Error!",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+          }
+        } else {
+          Swal.fire("Error", "Please select a game to edit.", "error");
+        }
+      } catch (error) {
+        Swal.fire("Xảy ra lỗi", "Vui lòng thử lại sau", "error");
+      }
+    },
+    onFileChangeForCreate(event) {
+      this.selectedFileForCreate = event.target.files[0];
+    },
+    async createNewGame() {
+      try {
+        const response = await GameService.addNewGame(
+          this.selectedFileForCreate,
+          this.nameGame
+        );
 
         if (response.status === 200) {
           await this.getAllGames(); // Assuming you have an `getAllGames` method to refresh the game list
@@ -244,18 +319,18 @@ export default {
       try {
         // Gọi method deleteGame từ GameService
         await GameService.deleteGame(id);
-        
+
         // Sử dụng SweetAlert2 để hiển thị thông báo xóa thành công
         await Swal.fire({
-          icon: 'success',
-          title: 'Xóa thành công',
-          text: 'Game đã được xóa thành công!',
+          icon: "success",
+          title: "Xóa thành công",
+          text: "Game đã được xóa thành công!",
         });
-        
+
         // Sau khi xóa thành công, cập nhật danh sách game
         await this.getAllGames();
       } catch (error) {
-        console.error('Lỗi khi xóa game:', error);
+        console.error("Lỗi khi xóa game:", error);
       }
     },
     showImage(name, imageUrl) {
@@ -274,18 +349,14 @@ export default {
         console.error("Lỗi khi lấy danh sách trò chơi: ", error);
       }
     },
-   
-
-   
   },
   async created() {
     await this.getAllGames();
   },
   computed: {
     isFormValid() {
-      return this.nameGame && this.selectedFile;
+      return this.nameGame && this.selectedFileForCreate;
     },
-
-  }
+  },
 };
 </script>
--- ezd-admin/src/service/GameService.js
+++ ezd-admin/src/service/GameService.js
@@ -1,15 +1,21 @@
 import axios from 'axios';
 
+const axiosInstance = axios.create({
+    headers: {
+      'Cache-Control': 'no-cache',
+    },
+  });
 const GAME_API_BASE_URL = 'http://localhost:8081/api/games/';
 
 class GameService {
+    
     async getAllGames() {
-        return await axios.get(GAME_API_BASE_URL);
+        return await axiosInstance.get(GAME_API_BASE_URL);
     }
 
     async deleteGame(id) {
         const url = `${GAME_API_BASE_URL}delete/${id}`;
-        return await axios.delete(url);
+        return await axiosInstance.delete(url);
     }
 
  
@@ -17,20 +23,20 @@ class GameService {
         const formData = new FormData();
         formData.append('imageName', file);
         formData.append('nameGame', nameGame);
-        return await axios.post(GAME_API_BASE_URL + 'add', formData);
+        return await axiosInstance.post(GAME_API_BASE_URL + 'add', formData);
     }
-
+   
     async editGame(id, file, nameGame) {
         const formData = new FormData();
-        formData.append('file', file);
+        formData.append('imageName', file);
         formData.append('nameGame', nameGame);
         
+        const url = `${GAME_API_BASE_URL}edit/${id}`;
         
-          const response = await axios.post(`${GAME_API_BASE_URL}edit/${id}`, formData);
-          return response.data;
-
-      }
-      
+        return await axiosInstance.put(url, formData);
+    }
+    
+   
 
 
 }
